13b2d6b7325f7eaca3a2b49b27892e67d0dbed90
seaweedbrain <nathanstchepinsky@gmail.com>
2025-03-05 23:07:19 -0500

    use creation date to compare result

--- source/JellyfinAPI.py
+++ source/JellyfinAPI.py
@@ -1,5 +1,6 @@
 from source import configuration
 import requests
+import datetime as dt
 
 def get_root_items():
     headers = {
@@ -9,16 +10,29 @@ def get_root_items():
     response = requests.get(f'{configuration.conf.jellyfin.url}/Items', headers=headers)
     if response.status_code != 200:
         raise Exception(f"Error while getting the root items, status code: {response.status_code}. Answer: {response.json()}.")
-    return response.json()
+    return response.json()["Items"]
 
 
-def get_item_from_parent(parent_id):
+def get_item_from_parent(parent_id, type, minimum_creation_date=None):
+    if type not in ["movie", "tv"]:
+        raise Exception(f"Error while retrieving a media from Jellyfin. Type must be 'movie' or 'tv'. Got {type}")
     headers = {
         "Authorization": f'MediaBrowser Token="{configuration.conf.jellyfin.api_token}"'
     }
 
-    response = requests.get(f'{configuration.conf.jellyfin.url}/Items?ParentId={parent_id}&fields=DateCreated', headers=headers)
+
+
+    response = requests.get(f'{configuration.conf.jellyfin.url}/Items?ParentId={parent_id}&fields=DateCreated&Recursive=true', headers=headers)
     if response.status_code != 200:
         raise Exception(f"Error while getting the items from parent, status code: {response.status_code}. Answer: {response.json()}.")
-
-    return response.json()
+    
+    
+    if not minimum_creation_date:
+        return response.json()["Items"]
+    else:
+        recent_items = []
+        for item in response.json()["Items"]:
+            creation_date = dt.datetime.strptime(item["DateCreated"].split("T")[0], "%Y-%m-%d")
+            if creation_date > minimum_creation_date:
+                recent_items.append(item)
+        return recent_items
