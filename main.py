7b7e559b849561dc884128b6fa12912ed519d6ac
seaweedbraincy <nathanstchepinsky@gmail.com>
2025-04-01 14:42:39 +0000

    Add safeguard in case of empty infos

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import logging
 
 
 
+
+
 if __name__ == "__main__":
     print("Welcome to jellyfin automatic newsletter !\n")
     print("Developped by Seaweedbrain, under MIT License.")
@@ -31,6 +33,17 @@ if __name__ == "__main__":
         items, total_count = JellyfinAPI.get_item_from_parent(parent_id=folder_id,type="movie", minimum_creation_date=dt.datetime.now() - dt.timedelta(days=configuration.conf.jellyfin.observed_period_days))
         total_movie += total_count
         for item in items:
+            if "ProductionYear"  not in item.keys():
+                logging.warning(f"Item {item['Name']} has no production year.")
+                item["ProductionYear"] = 0
+            if "DateCreated" not in item.keys():
+                logging.warning(f"Item {item['Name']} has no creation date.")
+                item["DateCreated"] = None
+            if "Overview" not in item.keys():
+                logging.warning(f"Item {item['Name']} has no overview.")
+                item["Overview"] = "No overview available."
+
+
             tmdb_info = TmdbAPI.get_media_detail_from_title(title=item["Name"], type="movie", year=item["ProductionYear"])
 
             movie_items[item["Name"]] = {
@@ -49,12 +62,22 @@ if __name__ == "__main__":
                 if (item['SeriesName'] in series_items.keys() and item["SeasonName"] not in series_items[item["SeriesName"]]["seasons"] ) or (item['SeriesName'] not in series_items.keys()):
 
                     if item['SeriesName'] not in series_items.keys() :
+                        if "DateCreated" not in item.keys():
+                            logging.warning(f"Item {item['SeriesName']} has no creation date.")
+                            item["DateCreated"] = None
+                        if "ProductionYear" not in item.keys():
+                            logging.warning(f"Item {item['SeriesName']} has no production year.")
+                            item["ProductionYear"] = 0
+                        
                         series_items[item['SeriesName']] = {
-                        "seasons": [item["SeasonName"]],
+                        "seasons": [item["SeasonName"]] if item["SeasonName"] else [],
                         "created_on": item["DateCreated"],
                         }
                         tmdb_info = TmdbAPI.get_media_detail_from_title(title=item["SeriesName"], type="tv", year=item["ProductionYear"])
-                        series_items[item['SeriesName']]["description"] = tmdb_info["overview"]
+                        if "overview" not in tmdb_info.keys():
+                            logging.warning(f"Item {item['SeriesName']} has no overview.")
+                            tmdb_info["Overview"] = "No overview available."
+                        series_items[item['SeriesName']]["description"] = tmdb_info["overview"] 
                         series_items[item['SeriesName']]["year"] = item["ProductionYear"]
                         series_items[item['SeriesName']]["poster"] = f"https://image.tmdb.org/t/p/w500{tmdb_info['poster_path']}" if tmdb_info["poster_path"] else "https://redthread.uoregon.edu/files/original/affd16fd5264cab9197da4cd1a996f820e601ee4.png"
                     else:
