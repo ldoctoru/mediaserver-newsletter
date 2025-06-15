4ea0ba7a8029ee09734fbe17ed13ab4825c08df0
guniv <13756122+guniv@users.noreply.github.com>
2025-06-15 15:56:51 -0400

    attempt to fix series spacing

--- source/email_template.py
+++ source/email_template.py
@@ -27,82 +27,76 @@ translation = {
 
 def populate_email_template(movies, series, total_tv, total_movie) -> str:
     with open("./template/new_media_notification.html") as template:
-        template=''.join(line.rstrip() for line in template)
-        if configuration.conf.email_template.language in ["fr", "en"]:
-            for key in translation[configuration.conf.email_template.language]:
-                template = re.sub(r"\${"+key+"}", translation[configuration.conf.email_template.language][key], template)
+        template = ''.join(line.rstrip() for line in template)
+        lang = configuration.conf.email_template.language
+        if lang in ["fr", "en"]:
+            for key in translation[lang]:
+                template = re.sub(r"\${" + key + "}", translation[lang][key], template)
         else:
-            raise Exception(f"[FATAL] Language {configuration.conf.email_template.language} not supported. Supported languages are fr and en")
+            raise Exception(f"[FATAL] Language {lang} not supported. Supported languages are fr and en")
 
         custom_keys = [
-            {"key":"title", "value":configuration.conf.email_template.title}, 
-            {"key":"subtitle", "value":configuration.conf.email_template.subtitle},
-            {"key":"jellyfin_url", "value":configuration.conf.email_template.jellyfin_url},
-            {"key":"jellyfin_owner_name", "value":configuration.conf.email_template.jellyfin_owner_name},
-            {"key":"unsubscribe_email", "value":configuration.conf.email_template.unsubscribe_email}
+            {"key": "title", "value": configuration.conf.email_template.title},
+            {"key": "subtitle", "value": configuration.conf.email_template.subtitle},
+            {"key": "jellyfin_url", "value": configuration.conf.email_template.jellyfin_url},
+            {"key": "jellyfin_owner_name", "value": configuration.conf.email_template.jellyfin_owner_name},
+            {"key": "unsubscribe_email", "value": configuration.conf.email_template.unsubscribe_email}
         ]
         for key in custom_keys:
-            template = re.sub(r"\${"+key["key"]+"}", key["value"], template)
+            template = re.sub(r"\${" + key["key"] + "}", key["value"], template)
 
-        if len(movies.keys())>=1:
-            template = re.sub(r"\${display_movies}", "", template) 
-            movies_html=""
-            for movie_title in movies.keys():
+        if movies:
+            template = re.sub(r"\${display_movies}", "", template)
+            movies_html = ""
+            for title, data in movies.items():
                 movies_html += f"""
                 <div class="movie_container">
-            <div class="movie_bg" style="background: url({movies[movie_title]["poster"]}) no-repeat; background-size: cover; background-position-y: center; ">
-                <table class="movie" >
-                <tr>
-                <td class="img">
-                    <img src="{movies[movie_title]["poster"]}" alt="Image du film" >
-                </td>
-                <td class="info">
-                    <h3>{movie_title}</h3>
-                    <span class="added">{translation[configuration.conf.email_template.language]["added_on"]} {movies[movie_title]["created_on"].split("T")[0]} </span>
-                    <p class="description">{movies[movie_title]["description"]}</p>
-                        
-                    </td>
-                    <tr>
-                </table>
-            </div>
-        </div>
-        """
+                    <div class="movie_bg" style="background: url({data['poster']}) no-repeat; background-size: cover; background-position-y: center;">
+                        <table class="movie">
+                            <tr>
+                                <td class="img">
+                                    <img src="{data['poster']}" alt="Movie poster">
+                                </td>
+                                <td class="info">
+                                    <h3>{title}</h3>
+                                    <span class="added">{translation[lang]['added_on']} {data['created_on'].split('T')[0]}</span>
+                                    <p class="description">{data['description']}</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                """
             template = re.sub(r"\${films}", movies_html, template)
-
         else:
-            template = re.sub(r"\${display_movies}", "display:none", template) 
+            template = re.sub(r"\${display_movies}", "display:none", template)
 
-        if len(series.keys())>=1:
-            template = re.sub(r"\${display_tv}", "", template) 
-            series_html=""
-            for serie_title in series.keys():
+        if series:
+            template = re.sub(r"\${display_tv}", "", template)
+            series_html = ""
+            for title, data in series.items():
                 series_html += f"""
                 <div class="movie_container">
-            <div class="movie_bg" style="background: url({series[serie_title]["poster"]}) no-repeat; background-size: cover; background-position-y: center; ">
-                <table class="movie" >
-                <tr>
-                <td class="img">
-                    <img src="{series[serie_title]["poster"]}" alt="Image du film" >
-                </td>
-                <td class="info">
-                    <h3>{serie_title} {", ".join(series[serie_title]["seasons"])}</h3>
-                    <span class="added">{translation[configuration.conf.email_template.language]["added_on"]} {series[serie_title]["created_on"].split("T")[0]} </span>
-                    <p class="description">{series[serie_title]["description"]}</p>
-                        
-                    </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        """
+                    <div class="movie_bg" style="background: url({data['poster']}) no-repeat; background-size: cover; background-position-y: center;">
+                        <table class="movie">
+                            <tr>
+                                <td class="img">
+                                    <img src="{data['poster']}" alt="Series poster">
+                                </td>
+                                <td class="info">
+                                    <h3>{title} {", ".join(data['seasons'])}</h3>
+                                    <span class="added">{translation[lang]['added_on']} {data['created_on'].split('T')[0]}</span>
+                                    <p class="description">{data['description']}</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                """
             template = re.sub(r"\${tvs}", series_html, template)
         else:
-            template = re.sub(r"\${display_tv}", "display:none", template) 
+            template = re.sub(r"\${display_tv}", "display:none", template)
 
         template = re.sub(r"\${series_count}", str(total_tv), template)
         template = re.sub(r"\${movies_count}", str(total_movie), template)
         return template
-
-
-
-
