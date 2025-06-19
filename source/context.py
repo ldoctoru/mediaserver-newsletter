5de37ddb95f4fcf45c7cd20125c586f648c97df6
seaweedbraincy <nathanstchepinsky@gmail.com>
2025-06-19 14:48:15 +0000

    Add safe disct class to ignore keyerror while formatting strings

--- source/context.py
+++ source/context.py
@@ -8,13 +8,21 @@ import datetime as dt
 from source import configuration
 import locale
 
+class SafeFormatDict(dict):
+    """
+    A dictionary that allows safe formatting of strings with placeholders.
+    If a key is not found, it returns a placeholder string instead of raising a KeyError.
+    """
+    def __missing__(self, key): 
+        return key.join("{}")
+
 # Set locale to the user's locale
 if configuration.conf.email_template.language == "fr":
     locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
 else:
     locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
 
-placeholders = {
+placeholders = SafeFormatDict({
     "date": dt.datetime.now().strftime("%Y-%m-%d"),
     "day_name": dt.datetime.now().strftime("%A"),
     "day_number": dt.datetime.now().strftime("%d"),
@@ -28,4 +36,4 @@ placeholders = {
     "start_month_number": (dt.datetime.now() - dt.timedelta(days=configuration.conf.jellyfin.observed_period_days)).strftime("%m"),
     "start_year": (dt.datetime.now() - dt.timedelta(days=configuration.conf.jellyfin.observed_period_days)).strftime("%Y")
 
-}
+})
