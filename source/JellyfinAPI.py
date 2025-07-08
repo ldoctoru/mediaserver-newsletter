797e854ee1f0d80aafa9ab8571d7bebf055072fb
seaweedbraincy <nathanstchepinsky@gmail.com>
2025-07-08 20:18:20 +0000

    Add debug logs

--- source/JellyfinAPI.py
+++ source/JellyfinAPI.py
@@ -1,13 +1,13 @@
-from source import configuration
+from source.configuration import conf, logging
 import requests
 import datetime as dt
 
 def get_root_items():
     headers = {
-        "Authorization": f'MediaBrowser Token="{configuration.conf.jellyfin.api_token}"'
+        "Authorization": f'MediaBrowser Token="{conf.jellyfin.api_token}"'
     }
 
-    response = requests.get(f'{configuration.conf.jellyfin.url}/Items', headers=headers)
+    response = requests.get(f'{conf.jellyfin.url}/Items', headers=headers)
     if response.status_code != 200:
         logging.error(f"Error while getting the root items, status code: {response.status_code}.")
         raise Exception(f"Error while getting the root items, status code: {response.status_code}. Answer: {response.text}.")
@@ -18,12 +18,12 @@ def get_item_from_parent(parent_id, type, minimum_creation_date=None):
     if type not in ["movie", "tv"]:
         raise Exception(f"Error while retrieving a media from Jellyfin. Type must be 'movie' or 'tv'. Got {type}")
     headers = {
-        "Authorization": f'MediaBrowser Token="{configuration.conf.jellyfin.api_token}"'
+        "Authorization": f'MediaBrowser Token="{conf.jellyfin.api_token}"'
     }
 
 
 
-    response = requests.get(f'{configuration.conf.jellyfin.url}/Items?ParentId={parent_id}&fields=DateCreated,ProviderIds&Recursive=true', headers=headers)
+    response = requests.get(f'{conf.jellyfin.url}/Items?ParentId={parent_id}&fields=DateCreated,ProviderIds&Recursive=true', headers=headers)
     if response.status_code != 200:
         logging.error(f"Error while getting the items from parent, status code: {response.status_code}.")
         raise Exception(f"Error while getting the items from parent, status code: {response.status_code}. Answer: {response.text}.")
@@ -34,15 +34,17 @@ def get_item_from_parent(parent_id, type, minimum_creation_date=None):
         for item in response.json()["Items"]:
             creation_date = dt.datetime.strptime(item["DateCreated"].split("T")[0], "%Y-%m-%d")
             if creation_date > minimum_creation_date:
+                logging.debug(f"Item {item['Name']} is more recent than {minimum_creation_date} (added on {creation_date}). Adding it to the list.")
+                logging.debug("Item details: " + str(item))
                 recent_items.append(item)
         return recent_items, response.json()["TotalRecordCount"]
 
 
 def get_item_from_parent_by_name(parent_id, name):
     headers = {
-        "Authorization": f'MediaBrowser Token="{configuration.conf.jellyfin.api_token}"'
+        "Authorization": f'MediaBrowser Token="{conf.jellyfin.api_token}"'
     }
-    response = requests.get(f'{configuration.conf.jellyfin.url}/Items?ParentId={parent_id}&fields=DateCreated,ProviderIds&Recursive=true', headers=headers)
+    response = requests.get(f'{conf.jellyfin.url}/Items?ParentId={parent_id}&fields=DateCreated,ProviderIds&Recursive=true', headers=headers)
     if response.status_code != 200:
         logging.error(f"Error while getting the items from parent, status code: {response.status_code}.")
         raise Exception(f"Error while getting the items from parent, status code: {response.status_code}. Answer: {response.text}.")
